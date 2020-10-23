96e6154418a212d31675aa9d39cef476e293a2ea
BirdManIkioiShota <kasukkun@gmail.com>
2020-10-23 12:58:12 +0900

    20201023 13:00

--- graphGenerator.py
+++ graphGenerator.py
@@ -3,22 +3,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime as dt
 
 # DojinHakushoをご利用いただきありがとうございます．
 # 本ツールはDLsiteをクロールし，サークル・声優の出演作情報を取得，図表としてレポートする機能を持ちます．
 # 現在は販売中の男性向けオーディオファイルに対応しています．
 # 詳細につきましては以下URLをご参照ください．
 # https://github.com/birdManIkioiShota/DoujinHakusho
-
-# ここに調べたいサークルの名前を入力する．
-# 名前はDLsiteのサークル名と一致させる必要がある
-# サークル不問の場合は空欄にすること
-circle_name = ""
-
-# ここに調べたい声優の名前を入力する．
-# 名前はDLsiteのクリエイタータグと一致させる必要がある．
-# 声優不問の場合は空欄にすること
-creator_name = "砂糖しお"
 
 data = pd.read_csv("data/raw_data.csv", index_col='id')
 #data = data[["price", "sales"]]
@@ -27,11 +18,16 @@ data = pd.read_csv("data/raw_data.csv", index_col='id')
 data['date'] = pd.to_datetime(data['date'])
 
 #data = data[['sales']]
-fig, axes = plt.subplots(figsize = (12, 8))
-plt.boxplot(data['sales'])
-axes.set_xticklabels(['2016-01-23 ~ 2020-10-22'])
-plt.grid()
-plt.show()
+
+# 箱ひげ図
+#fig, axes = plt.subplots(figsize = (12, 8))
+#plt.boxplot(data['sales'])
+#plt.title('box plot')
+#plt.ylabel('sales')
+#plt.ylim([0,10000])
+#axes.set_xticklabels(['2016-01-23 ~ 2020-10-22'])
+#plt.grid()
+#plt.show()
 
 # 年ごとの統計
 #data = data[['date', 'sales']]
@@ -43,3 +39,10 @@ plt.show()
 #plt.hist(data['sales'], bins=np.logspace(1,5,int(np.sqrt(len(data)))))
 #plt.gca().set_xscale("log")
 #plt.show()
+
+
+#data_by_y = data[(data['date'] >= dt.datetime(2000,1,1)) & (data['date'] <= dt.datetime(2000,12,31))]
+#print(data_by_y.describe())
+#if data_by_y.empty:
+#    print('empty!')
+
